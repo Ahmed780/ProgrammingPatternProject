6f96eff85f6b107af0077ba34be93d2b920bfb6c
UzairLakhani <lakhaniuzair780@gmail.com>
2021-05-15 01:02:45 -0400

    Update FlightController.java

--- Final project/FlightController.java
+++ Final project/FlightController.java
@@ -17,21 +17,72 @@ public class FlightController {
         this.view = view;
     }
     
-    public void createFlightsTable(String sqlStatement) {
+    public void createFlightsTable(String createTable) {
         try (Connection con = DBConnection.DbConnector();
                 Statement stmt = con.createStatement()){
-            stmt.executeUpdate("DROP TABLE IF EXIST Flights;");
-            stmt.executeUpdate(sqlStatement); 
+          stmt.executeUpdate("DROP TABLE IF EXISTS Flights;");
+            stmt.executeUpdate(createTable); 
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
-    
+      public boolean addFlight(Flight flight) {
+        try (Connection con = DBConnection.DbConnector();
+                Statement stmt = con.createStatement()) {
+           
+            String createTable = "INSERT INTO Flights (flightN, name, origin, dest, "
+                    + "duration, seats, available, amount) "
+                    + "VALUES (" + flight.getFlightN() + ", '" + flight.getName() + "',' "
+                    + flight.getOrigin() + "',' " + flight.getDest() + "',' "
+                    + flight.getDuration() + "',' " + flight.getSeats() + "',' "
+                    + flight.isAvailable() + "',' " + flight.getAmount() + "');";
+            System.out.println("Inserting data " + createTable);
+            stmt.executeUpdate(createTable);
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        return false;
+    }
+        public boolean removerFlight(String flightN) {
+          try (Connection con = DBConnection.DbConnector();
+                Statement stmt = con.createStatement()) {           
+            String createTable = "DELETE FROM Flights WHERE flightN= '1001';"+ flightN;
+              System.out.println("\ndelete from Flights where flightN");
+            stmt.execute(createTable);
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+        return false;
+    }
+        public Map<Integer, String> getAllFlights() {
+        Map<Integer, String> map = new HashMap();
+
+        try (Connection con = DBConnection.DbConnector();
+         Statement stmt = con.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT * from Flights;");
+            while (rs.next()) {
+                //int id = rs.getInt("STUDENTID");
+                String name = rs.getString("name");
+                String addr = rs.getString("origin");
+                map.put(rs.getInt("flightN"), " name: " + name + ", origin: " + addr);
+            }
+               } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        return map;
+    }
+            
     public void createClientsTable(String sqlStatement) {
          try (Connection con = DBConnection.DbConnector();
                 Statement stmt = con.createStatement()){
-            stmt.executeUpdate("DROP TABLE IF EXIST Clients;");
+            stmt.executeUpdate("DROP TABLE IF EXISTS Clients;");
             stmt.executeUpdate(sqlStatement); 
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
@@ -42,7 +93,7 @@ public class FlightController {
     public void createReservedFlightsTable(String sqlStatement) {
          try (Connection con = DBConnection.DbConnector();
                 Statement stmt = con.createStatement()){
-            stmt.executeUpdate("DROP TABLE IF EXIST ReservedFlights;");
+            stmt.executeUpdate("DROP TABLE IF EXISTS ReservedFlights;");
             stmt.executeUpdate(sqlStatement); 
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
