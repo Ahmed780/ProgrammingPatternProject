6a7facbf5962178887671296b3e6de6f76ebf56a
UzairLakhani <lakhaniuzair780@gmail.com>
2021-05-15 19:34:01 -0400

    Add files via upload

---
+++ Final project/ClientView.java
@@ -0,0 +1,13 @@
+package progproject;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Uzair Lakhani, Muhammad Val
+ */
+public class ClientView {
+    public static void printClientDetails(Map map) {
+        System.out.println(map);
+    }
+}
