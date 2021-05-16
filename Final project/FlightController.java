01d0d28ce4e0cebd380efdeb374b08f130906aa5
UzairLakhani <lakhaniuzair780@gmail.com>
2021-05-16 00:31:45 -0400

    Removed useless methods

--- Final project/FlightController.java
+++ Final project/FlightController.java
@@ -26,65 +26,7 @@ public class FlightController {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-    }
-      public boolean addFlight(Flight flight) {
-        try (Statement stmt = con.createStatement()) {         
-            String createTable = "INSERT INTO Flights (flightN, name, origin, dest, "
-                    + "duration, seats, available, amount) "
-                    + "VALUES (" + flight.getFlightN() + ", '" + flight.getName() + "',' "
-                    + flight.getOrigin() + "',' " + flight.getDest() + "',' "
-                    + flight.getDuration() + "',' " + flight.getSeats() + "',' "
-                    + flight.isAvailable() + "',' " + flight.getAmount() + "');";
-            System.out.println("Inserting data " + createTable);
-            stmt.executeUpdate(createTable);
-            return true;
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-        return false;
-    }
-        public boolean removerFlight(String flightN) {
-          try (Statement stmt = con.createStatement()) {           
-            String createTable = "DELETE FROM Flights WHERE flightN= '1001';"+ flightN;
-              System.out.println("\ndelete from Flights where flightN");
-            stmt.execute(createTable);
-            return true;
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-
-        return false;
-    }
-        public Map<Integer, String> getAllFlights() {
-        Map<Integer, String> map = new HashMap();
-
-        try (Statement stmt = con.createStatement()) {
-            ResultSet rs = stmt.executeQuery("SELECT * from Flights;");
-            while (rs.next()) {
-                //int id = rs.getInt("STUDENTID");
-                String name = rs.getString("name");
-                String addr = rs.getString("origin");
-                map.put(rs.getInt("flightN"), " name: " + name + ", origin: " + addr);
-            }
-               } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-        return map;
-    }
-            
-    public void createClientsTable(String sqlStatement) {
-         try (Statement stmt = con.createStatement()){
-            stmt.executeUpdate("DROP TABLE IF EXISTS Clients;");
-            stmt.executeUpdate(sqlStatement); 
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-    }
-    
+    } 
     public void createReservedFlightsTable(String sqlStatement) {
         try (Statement stmt = con.createStatement()) {
             stmt.executeUpdate("DROP TABLE IF EXISTS ReservedFlights;");
